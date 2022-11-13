38aaf313d6b9de72fd7eda3d49749386d685775b
Your Name <you@example.com>
2022-11-12 20:25:27 -0700

    mvp frontend

--- app.py
+++ app.py
@@ -57,6 +57,26 @@ def patch_user_sensative():
     return user_sensative.user_sensative.patch()
 
 
+#------------------------- /api/change-password -------------------------#
+# change password
+
+@app.patch('/api/patch-password')
+def password_patch():
+    is_valid = check_endpoint_info(request.headers, ['token'])
+    is_valid_data = check_endpoint_info(request.json, ['password'])
+    if(is_valid != None or is_valid_data != None):
+        return make_response(json.dumps(is_valid, is_valid_data, default=str), 400)
+
+    results = run_statment('CALL user_get_sensative(?)', [request.headers.get('token')])
+    if(type(results) != list):
+        return make_response(json.dumps(results), 400)
+
+    results = run_statment('CALL patch_password(?,?)', [results[0]['password'], request.headers['token']])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
+
 #------------------------- /api/user-login -------------------------#
 # user-login
 
@@ -105,7 +125,7 @@ def upload_delete():
     return user_upload.user_upload.delete()
     
     
-#------------------------- /api/user-get-upload-image -------------------------#
+#------------------------- /api/get-upload-image -------------------------#
 
 # need another enpoint? or add more to it?  or use 2 axios requests? this get and upload get?
 @app.get('/api/user-upload-image')
@@ -114,13 +134,28 @@ def upload_get():
     if(is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = run_statment('CALL get_upload_image(?)', [request.args.get('upload_id')])
+    results = run_statment('CALL get_upload_image(?)', [request.args['upload_id']])
     if(type(results) != list):
         return make_response(json.dumps(results, default=str), 500)
     elif(len(results) == 0):
         return make_response(json.dumps("invalid image id"), 400)
 
-    return send_from_directory('images', results[0]['image_ref'])
+    return send_from_directory('images', results[0]['upload_image'])
+
+#------------------------- /api/get-meta-data -------------------------#
+
+
+@app.get('/api/upload-meta-data')
+def meta_data_get():
+    is_valid = check_endpoint_info(request.args, ['upload_id'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    results = run_statment('CALL get_meta_data(?)', [request.args['upload_id']])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
 
 #------------------------- /api/user-comments -------------------------#
 # GET all comments
@@ -195,16 +230,84 @@ def favourite_delete():
 #------------------------- /api/following -------------------------#
 # GET all people you're following
 
+@app.get('/api/following')
+def following_get():
+    is_valid = check_endpoint_info(request.args, ['token'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+    
+    results = run_statment('CALL get_following(?)', [request.args['token']])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
 
-# POST follow someone
+# POST follow someone  MIGHT BE WRONG NEED TO CHECK PROCEDURE
+# @app.post('/api/following')
+# def following_post():
+#     is_valid = check_endpoint_info(request.headers, ['token'])
+#     is_valid_data = check_endpoint_info(request.json, ['following_user_id', 'followed_by_user_id'])
+#     if(is_valid != None or is_valid_data != None):
+#         return make_response(json.dumps(is_valid, is_valid_data, default=str), 400)
+
+#     results = run_statment('CALL post_following(?,?,?)', [request.json['following_user_id'],request.json['followed_by_user_id'], request.headers['token']])
+#     if(type(results) == list):
+#         return make_response(json.dumps(results, default=str), 200)
+#     else:
+#         return make_response(json.dumps(results, default=str), 500)
 
 
 # DELETE stop following
 
+@app.delete('/api/following')
+def following_delete():
+    is_valid = check_endpoint_info(request.headers, ['token'])
+    is_valid_data = check_endpoint_info(request.json, ['following_user_id'])
+    if(is_valid != None or is_valid_data != None):
+        return make_response(json.dumps(is_valid, is_valid_data, default=str), 400)
+
+    results = run_statment('CALL delete_following(?,?)', [request.json['following_user_id'] ,request.headers['token']])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
 
 
 #------------------------- /api/followed-by -------------------------#
 # GET all people you're followed by
+@app.get('/api/followed-by')
+def followed_by_get():
+    is_valid = check_endpoint_info(request.args, ['token'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+    
+    results = run_statment('CALL get_followed_by(?)', [request.args['token']])
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
+
+
+#------------------------- /api/get-all-images -------------------------#
+
+@app.get('/api/get-all-images')
+def upload_get_all_images():
+    is_valid = check_endpoint_info(request.args, ['file_name'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+
+    return send_from_directory('images', request.args['file_name'])
+
+#------------------------- /api/get-all-uploads-meta -------------------------#
+
+@app.get('/api/get-all-uploads-meta')
+def upload_get_all_uploads_meta():
+    results = run_statment('CALL get_all_uploads_meta()')
+    if(type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps(results, default=str), 500)
 
 
 if(dbcreds.production_mode == True):
@@ -216,3 +319,37 @@ else:
     CORS(app)
     print("Running in Testing Mode!")
     app.run(debug=True)
+
+
+
+
+
+#  call post_favourite(6,'token');
+
+#  call get_favourite('919f568243a549ffbe14f53bc3e9af5a');
+
+#  call delete_favourite(6,'token');
+
+#  call post_tag('TAG!', 6,'919f568243a549ffbe14f53bc3e9af5a');
+
+#  call get_tags(7);
+
+#  call delete_tags(5,'token');
+
+#  call post_following(1,'919f568243a549ffbe14f53bc3e9af5a');
+
+#  call get_following('token');
+
+#  call delete_following(1,'919f568243a549ffbe14f53bc3e9af5a');
+
+#  call get_followed_by('token');
+ 
+# call test(1,'919f568243a549ffbe14f53bc3e9af5a');
+
+# call patch_password('passwords','token');
+
+# call get_upload_image(6);
+
+# call get_all_uploads();
+
+# call get_all_images();
